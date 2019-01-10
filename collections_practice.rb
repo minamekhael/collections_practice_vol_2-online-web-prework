e7b502e44827c20d08f3bf86c663db89abc8803a
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-10 22:38:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+def begins_with_r(element)
+  element.all? do |word|
+    word.start_with?("r")
+  end
+end
+
+def contain_a(element)
+ element.select {|i| i.include?("a")} 
+end
+
+def first_wa(element)
+  element.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(element)
+  element.delete_if { |obj| !(obj.is_a? String) }
+end
+
+def count_elements(element)
+  counts = Hash.new 0
+  element.each do |word|
+  counts[word] += 1
+end
+end
